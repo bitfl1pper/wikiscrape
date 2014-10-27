7a9d7f1f7ac6c2e3a79ade11ca1368aad3ff5ce6
bitfl1pper <bitfl1pper@users.noreply.github.com>
2014-10-27 00:04:18 -0400

    add get element fn

--- src/wikiscrape/core.clj
+++ src/wikiscrape/core.clj
@@ -6,3 +6,9 @@
 (defn parse-web-html
   [h]
   (html/html-resource (io/as-url h)))
+
+(defn get-element
+  "Pull out text by specific html tag.
+   e.g. :p for p tag, :h1 for h1, etc."
+  [element h]
+  (map html/text (html/select (parse-web-html h) [element])))
