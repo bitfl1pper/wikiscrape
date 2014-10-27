7ccef58c341aabf0ac456a686d311bd5a6a7f3cb
bitfl1pper <bitfl1pper@users.noreply.github.com>
2014-10-26 23:59:18 -0400

    add web html parse fn, update core ns deps

--- src/wikiscrape/core.clj
+++ src/wikiscrape/core.clj
@@ -1,2 +1,7 @@
 (ns wikiscrape.core
-  [net.cgrand.enlive-html :as html])
+  [net.cgrand.enlive-html :as html]
+  [clojure.java.io :as io])
+
+(defn parse-web-html
+  [h]
+  (html/html-resource (io/as-url h)))
