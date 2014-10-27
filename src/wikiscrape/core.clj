ec4a9663931931a1b268fef2f17245bf9275b160
bitfl1pper <bitfl1pper@users.noreply.github.com>
2014-10-27 00:32:31 -0400

    add usage example script

--- src/wikiscrape/core.clj
+++ src/wikiscrape/core.clj
@@ -17,3 +17,38 @@
   [title]
   (let [url (str "https://en.wikipedia.org/wiki/" title)]
     (apply str (get-element :p url))))
+
+;;;;; Example Usage ;;;;;
+
+;;; here's a simple script to get all text from (e.g.) one of wikipedias 'featured'
+;;; areas.
+;;; in this example we are using the physics section because the wikipedia community
+;;; has decided that it is exemplary of wikipedia at its best.
+;;;
+;;; the physics category:
+;;; it has a basic tree structure with physics at the top, four or so sub categories,
+;;; and each topic in each category at the bottom
+;;;
+;;; we will naively represent this ontology(?) with a simple data structure called a
+;;; 'plex' (placeholder name)
+;;; Make a 'plex' data structure, the name of the plex is the
+;;; overall category, each key is a sub category and is paired with a vector
+;;; containing strings corresponding to topics in that sub category
+
+(def physics-plex
+  {:fundamental-concepts ["Space" "Time" "Matter" "Energy" "Electromagnetism" "Strong_interaction" "Weak_interaction" "Gravitational_Field" "Entropy"]
+   :classical-physics ["Classical_Mechanics" "Electromagnetism" "States_of_Matter" "Thermodynamics" "Solid_Mechanics" "Fluid_Mechanics" "Acoustics" "Optics"]
+   :modern-physics ["Special_Relativity" "General_Relativity" "Quantum_Mechanics" "Quantum_Field_Theory" "Quantum_Gravity" "Condensed_Matter_Physics" "Atomic_Physics" "Nuclear_Physics" "Particle_Physics" "Plasma_Physics"]
+   :cross-discipline-topics ["Mathematical_Physics" "Astronomy" "Chemical_Physics"  "Materials_Science" "Geophysics" "Biophysics" "Nanotechnology"]})
+
+(defn flat-plex
+  "This flattens the plex (and removes redundant topics between subcategories) so that
+   it can be processed by other functions. It does not destroy or mutate the plex,
+   so that it can maintain the hierarchical information, for further use."
+  [plex]
+  (distinct (flatten (map #(% plex) (keys plex)))))
+
+(defn scrape-wiki-plex
+  "This maps the bodytext scrape fn through a plex, it returns a lot of text."
+  [plex]
+  (map #(wikipage-bodytext %) (flat-plex plex)))
